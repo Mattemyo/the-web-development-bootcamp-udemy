045423569fec0ac58c45ce1cf0503339181c1ade
Mattemyo <matias97marti@gmail.com>
2017-12-30 10:50:01 +0100

    Respond to post request with redirect

--- Section 24 - Intermediate Express/MoreExpress/PostRequestDemo/app.js
+++ Section 24 - Intermediate Express/MoreExpress/PostRequestDemo/app.js
@@ -2,27 +2,36 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 // shortcut
 app.set('view engine', 'ejs');
 
+// array of friends
+const friends = [
+    'Tony', 'Miranda', 'Justin', 'Pierre', 'Lily'
+];
 // Home page
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('home');
 });
 // add new friend to the array
-app.post('/addfriend', function(req, res) {
-    res.send('post route');
+app.post('/addfriend', function (req, res) {
+    // push input value into friends array
+    friends.push(req.body.newfriend);
+    res.redirect('/friends');
 });
 // friends form
-app.get('/friends', function(req, res) {
-    const friends = [
-        'Tony', 'Miranda', 'Justin', 'Pierre', 'Lily'];
-    res.render('friends', {friends: friends});
+app.get('/friends', function (req, res) {
+
+    res.render('friends', {
+        friends: friends
+    });
 });
 
 
 // Localhost
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('App listening on port 3000!');
 });
