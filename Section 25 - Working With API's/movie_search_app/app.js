bf9e61ce623a89d9b474549fdb0b41213f308a63
Mattemyo <matias97marti@gmail.com>
2017-12-30 12:21:33 +0100

    Turn search term into variable

--- Section 25 - Working With API's/movie_search_app/app.js
+++ Section 25 - Working With API's/movie_search_app/app.js
@@ -2,15 +2,21 @@ const express = require("express");
 const app = express();
 const request = require("request");
 app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('search');
+});
+
 app.get("/results", function(req, res) {
-  request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(
+    const query = req.query.search
+  request(`http://www.omdbapi.com/?s=${query}&apikey=thewdb`, function(
     error,
     response,
     body
   ) {
     if (!error && response.statusCode === 200) {
         const data = JSON.parse(body);
-      res.send(data.Search[0].Title);
+        res.render('results', {data: data});
     }
   }); //request
 }); //app.get
