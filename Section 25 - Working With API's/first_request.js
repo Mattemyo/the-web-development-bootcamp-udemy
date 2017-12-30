fb69fa4eca63c5d6c9d2b4c42aa27feb3614f49a
Mattemyo <matias97marti@gmail.com>
2017-12-30 11:28:28 +0100

    Parse JSON

--- Section 25 - Working With API's/first_request.js
+++ Section 25 - Working With API's/first_request.js
@@ -1,8 +1,11 @@
 const request = require("request");
 
-request("http://www.google.com", function(error, response, body) {
-  if(!error && response.statusCode == 200){
-          console.log(body);
-      }
+request(
+  "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+  function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        const data = JSON.parse(body);
+      console.log(data.query.results.channel.astronomy.sunset);
+    }
   }
-});
+);
