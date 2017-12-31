c3be804ad67f6c255932f9124186e9cf20501d6d
Mattemyo <matias97marti@gmail.com>
2017-12-31 01:57:39 +0100

    Connect user input to database

--- Section 28 - YelpCamp - Data Persistance/v2/app.js
+++ Section 28 - YelpCamp - Data Persistance/v2/app.js
@@ -1,48 +1,71 @@
-const express = require("express");
-const app = express();
-// in addition to express
-const bodyParser = require("body-parser");
+const express = require("express"),
+  app = express(),
+  // in addition to express
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({ extended: true }));
-
 // shorten file names
 app.set("view engine", "ejs");
+
+//insert schema
+// Schema is like bluerprint
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Salmon Creek",
+//     image:
+//       "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?h=350&auto=compress&cs=tinysrgb"
+//   },
+//   (err, campgrounds) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("campground", campgrounds);
+//     }
+//   }
+// );
+
 // landing page
 app.get("/", function(req, res) {
   res.render("landing");
 });
-//array of campgrounds
-const campgrounds = [
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?h=350&auto=compress&cs=tinysrgb"
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://images.pexels.com/photos/618848/pexels-photo-618848.jpeg?h=350&auto=compress&cs=tinysrgb"
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image:
-      "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?h=350&auto=compress&cs=tinysrgb"
-  }
-];
 // campgrounds page
 app.get("/campgrounds", function(req, res) {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  //get all campgrounds from DB
+  Campground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds", { campgrounds: allCampgrounds });
+    }
+  });
+
+  // res.render("campgrounds", { campgrounds: campgrounds });
 });
 // add new post route
 app.post("/campgrounds", function(req, res) {
   //add form data to campgrounds array
-  campgrounds.push({
-    name: req.body.name,
-    image: req.body.image
-  });
-
-  //redirect back to campgrounds page
-  res.redirect("/campgrounds");
+  Campground.create(
+    {
+      name: req.body.name,
+      image: req.body.image
+    },
+    (err, newlyCreated) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/campgrounds");
+      }
+    }
+  );
 });
 //page for adding new campgrounds
 app.get("/campgrounds/new", function(req, res) {
---
+++ Section 28 - YelpCamp - Data Persistance/v2/readme.md
@@ -0,0 +1,30 @@
+#YelpCamp
+
+*Add Landing Page
+*Add Campgrounds Page that lists all campgrounds
+
+Each Campground has:
+*Name
+*Image
+
+
+#Layout and Basic Styling
+
+*Create header and footer partials
+*Bootstrap
+
+#Creating New Campgrounds
+*Setup new campground POST route
+*Add in body-parser
+*Setup rout to show form
+*Add basic unstyled form
+
+#Style campgrounds page
+
+*Add a better header/title
+*Make campgrounds in grid display
+
+#Style Navbar and Form
+*Add navbar to all templates
+*Style the new campgrounds form
+
