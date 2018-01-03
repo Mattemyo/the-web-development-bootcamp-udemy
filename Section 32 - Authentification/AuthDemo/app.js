dc9e95bda646aaba91b7574e2d63b09677b451ab
Mattemyo <matias97marti@gmail.com>
2018-01-03 16:55:31 +0100

    Create user and register page, redirect to secret page with authenticate

--- Section 32 - Authentification/AuthDemo/app.js
+++ Section 32 - Authentification/AuthDemo/app.js
@@ -1,17 +1,71 @@
+//packages
 const express = require("express"),
-  mongoose = require("mongoose");
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  bodyParser = require("body-parser"),
+  localStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose"),
+  //require models
+  User = require("./models/user");
 
+//connect to DB
 mongoose.connect("mongodb://localhost/auth_demo_app");
+
+//start express
 const app = express();
+//shorten file names
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//ACTIVATE PASSPORT
+app.use(
+  require("express-session")({
+    secret: "Aik e best",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// =================
+//ROUTES
+// =================
 
 // LANDING PAGE
 app.get("/", function(req, res) {
   res.render("home");
 });
-
+//secret
 app.get("/secret", function(req, res) {
   res.render("secret");
+});
+
+//Auth routes
+//show sign up form
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+//handle user signup
+app.post("/register", function(req, res) {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    function(err, user) {
+      if (err) {
+        console.error(err);
+        res.render("register");
+      } else {
+        passport.authenticate("local")(req, res, function() {
+          res.redirect("/secret");
+        });
+      }
+    }
+  );
 });
 
 app.listen(3000, function() {
