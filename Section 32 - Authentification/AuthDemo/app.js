8ea5230e2247e5fc7b38248522684c0d07c45803
Mattemyo <matias97marti@gmail.com>
2018-01-03 15:58:50 +0100

    Set up home and secret page, require express and mongoose

--- Section 32 - Authentification/AuthDemo/app.js
+++ Section 32 - Authentification/AuthDemo/app.js
@@ -1,15 +1,19 @@
-const express = require("express");
+const express = require("express"),
+  mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/auth_demo_app");
 const app = express();
 app.set("view engine", "ejs");
 
+// LANDING PAGE
 app.get("/", function(req, res) {
   res.render("home");
 });
 
-
-
+app.get("/secret", function(req, res) {
+  res.render("secret");
+});
 
 app.listen(3000, function() {
-    console.log('App listening on port 3000!');
+  console.log("App listening on port 3000!");
 });
