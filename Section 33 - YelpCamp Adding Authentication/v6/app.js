1207d0f01ff990b2b7ba9b8784fd16472b4f0ae2
Mattemyo <matias97marti@gmail.com>
2018-01-03 23:54:26 +0100

    Add logout route, check if user is logged in for posting comments

--- Section 33 - YelpCamp Adding Authentication/v6/app.js
+++ Section 33 - YelpCamp Adding Authentication/v6/app.js
@@ -90,7 +90,7 @@ app.get("/campgrounds/:id", function(req, res) {
 // =================
 //COMMENTS ROUTES
 // =================
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {
       console.log(err);
@@ -100,7 +100,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
   });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
   //lookup campground using ID
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {
@@ -158,6 +158,19 @@ app.post(
     //callback function
   }
 );
+//logic logout route
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
 // port 3000
 app.listen(3000, function() {
   console.log("App listening on port 3000!");
