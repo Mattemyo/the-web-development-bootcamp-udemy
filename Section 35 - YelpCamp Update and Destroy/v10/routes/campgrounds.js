fb8e2abb4b2097b9811d9c9669a06cd63fbbf65e
Mattemyo <matias97marti@gmail.com>
2018-01-08 00:59:46 +0100

    Add delete route and add buttons to show page

--- Section 35 - YelpCamp Update and Destroy/v10/routes/campgrounds.js
+++ Section 35 - YelpCamp Update and Destroy/v10/routes/campgrounds.js
@@ -85,6 +85,17 @@ router.put("/:id", function(req, res) {
   });
 });
 
+//DESROY CAMPGROUND ROUTE
+router.delete("/:id", function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
 //middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
