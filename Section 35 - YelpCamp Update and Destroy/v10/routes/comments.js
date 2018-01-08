d0dec245e79bdf46aeba3b40089fc0013f907193
Mattemyo <matias97marti@gmail.com>
2018-01-08 02:50:59 +0100

    Limit authority for comments

--- Section 35 - YelpCamp Update and Destroy/v10/routes/comments.js
+++ Section 35 - YelpCamp Update and Destroy/v10/routes/comments.js
@@ -40,7 +40,7 @@ router.post("/", isLoggedIn, function(req, res) {
   });
 });
 //EDIT ROUTE
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
   Comment.findById(req.params.comment_id, function(err, foundComment) {
     if (err) {
       res.redirect("back");
@@ -53,7 +53,7 @@ router.get("/:comment_id/edit", function(req, res) {
   });
 });
 // PUT ROUTE
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(
     err,
     updatedComment
@@ -66,7 +66,7 @@ router.put("/:comment_id", function(req, res) {
   });
 });
 //COMMENT DESTROY ROUTE
-router.delete("/:comment_id", function(req, res) {
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
   Comment.findByIdAndRemove(req.params.comment_id, function(err) {
     if (err) {
       res.redirect("back");
@@ -75,6 +75,26 @@ router.delete("/:comment_id", function(req, res) {
     }
   });
 });
+
+//authorization
+function checkCommentOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if (err) {
+        res.redirect("back");
+      } else {
+        //owner or not?
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
 
 //middleware
 function isLoggedIn(req, res, next) {
